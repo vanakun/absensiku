2c1d1fb4e8456b54b1b1912b90742a00f16a2d74
newbiedos <inidimas13@gmail.com>
2024-06-15 14:41:25 +0700

    intitial-commit

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -71,7 +71,7 @@
                   <p
                     class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400"
                   >
-                    Total Yang Absen Hari Ini
+                    Total Absen Hari Ini
                   </p>
                   <p
                     class="text-lg font-semibold text-gray-700 dark:text-gray-200"
